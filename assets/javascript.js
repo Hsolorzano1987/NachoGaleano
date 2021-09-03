ad3c3e4e576193daf769469c32945542f6f2b1c8
Hsolorzano <hernan.front@poliandino.edu.co>
2021-09-02 23:09:26 -0500

    avansep02

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,20 +1,38 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dots");
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
 
-    if(n > slides.length) {
-        slideIndex = 1;
-    }
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
 
-    if (n < 1){
-        slideIndex = slides.length;
-    }
-   
-  for (let i = 0; i < slides.length; i++){
-      slides[i].style.display = "none";  
-  }  
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className.replace(" active", "");
+  }
+slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+
+  slideIndex++;
+
+  setTimeout(() => {
+    showSlides(slideIndex);
+  }, 10000);
 }
